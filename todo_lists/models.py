1986c513b5ce3899c79491fbe6b3a679ba5c9726
George Hart <hamiltonhart@protonmail.com>
2020-04-27 21:48:28 -0700

    Calculate worth added to the TodoList model.

--- todo_lists/models.py
+++ todo_lists/models.py
@@ -9,5 +9,9 @@ class TodoList(models.Model):
     created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateField(default=datetime.date.today)
 
+    # Worth relates to worth assigned to each item.
+    # Setting calculate_worth to true indicates the worth of the items should be calculated for review
+    calculate_worth = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
